80cad85373e1e93cff9b0c037a0749123f7c959b
eraygundogmus <gundogmuseray@gmail.com>
2021-02-28 21:09:09 +0300

    commit to try deploy with zeit

--- components/moviecontainers.js
+++ components/moviecontainers.js
@@ -3,7 +3,7 @@ import React from 'react'
 function MovieContainer(data) {
     return (
         <div>
-            <h1> { data ? data.data[1].id : "Veri yok"} </h1>
+         { data ? data.data.map( e => (<h1 key={e.id}>{e.id}</h1>)) : "Veri yok"}
         </div>
     )
 }
