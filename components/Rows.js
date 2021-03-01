3b47a2cae04b736f9f9909b508c43ac3f926193d
eraygundogmus <gundogmuseray@gmail.com>
2021-03-01 21:18:00 +0300

    display card features on hover

--- components/Rows.js
+++ components/Rows.js
@@ -1,5 +1,6 @@
 import axios from '../components/base_url';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import PlayCircleFilledOutlinedIcon from '@material-ui/icons/PlayCircleFilledOutlined';
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
@@ -7,12 +8,19 @@ import ThumbUpAltRoundedIcon from '@material-ui/icons/ThumbUpAltRounded';
 import ThumbDownAltOutlinedIcon from '@material-ui/icons/ThumbDownAltOutlined';
 import ThumbDownAltRoundedIcon from '@material-ui/icons/ThumbDownAltRounded';
 import KeyboardArrowDownOutlinedIcon from '@material-ui/icons/KeyboardArrowDownOutlined';
+import styles from './Rows.module.scss'
 
 const img_url = "https://image.tmdb.org/t/p/w500/"
 
-function Rows( {title, url} ) {
+function Rows( {title, url, rowName} ) {
     const [request, setRequest] = useState([]);
-    const [features, setFeatures ] = useState(false)
+    const infoRef = useRef(null)
+    const [features, setFeatures ] = useState({            
+        border: '1px solid black',
+        width: '264px',
+        textAlign: 'center',
+        display: 'none'
+    })
 
     useEffect(() => {
         async function fetchRowItems() {
@@ -22,32 +30,46 @@ function Rows( {title, url} ) {
         }
         
         fetchRowItems();
-        console.log(request)
+//        console.log(request)
     },[url])
 
 // To show features, i need to add an onHover effect for every item which will change item's css to display features.
 // but the problem is, for every row and for every item, there should be a unique class so other's will not change.
 
-// solution is, giving different className for every row like row__container__horror!
+// solution is, giving different className for every row like row__container__horror! 
+
+
     function hoverHandler(e) {
-        e.target.style.width = '300px'
+        document.getElementsByClassName(`${e.target.nextSibling.className}`)[0].style.display = 'unset'
+        
     }
 
+    function hoverLeaveHandler(e) {
+        document.getElementsByClassName(`${e.target.nextSibling.className}`)[0].style.display = 'none'
+    }
+
+
     return (
-        <div className="row__container">
+        <div className={`${rowName}_container`}>
             <h2> {title} </h2>
-            {request.map((card) => (
-                <div key={card.id} className="row__container__card">
-                <div>{card.name ? card.name : card.title}</div>
-                <img /* onMouseEnter={hoverHandler} */ src={`${img_url}${card.poster_path}`}/>
-                <p> {card.overview} </p>
-                <PlayCircleFilledOutlinedIcon fontSize="large"/>
-                <AddCircleOutlineOutlinedIcon fontSize="large" />
-                <ThumbUpAltOutlinedIcon fontSize="large" />
-                <ThumbDownAltOutlinedIcon fontSize="large" />
-                <KeyboardArrowDownOutlinedIcon fontSize="large" />
+                <div className={styles.row}> 
+                    {request.map((card) => (
+                        <div key={card.id} className={styles.card}>
+                            <img onMouseEnter={hoverHandler} onMouseLeave={hoverLeaveHandler} className={styles.posters} src={`${img_url}${card.poster_path}`}/>
+                            <div className={card.id} style={features} ref={infoRef}>
+                                    <div className={styles.buttons}>
+                                        <PlayCircleFilledOutlinedIcon fontSize="large"/>
+                                        <AddCircleOutlineOutlinedIcon fontSize="large" />
+                                        <ThumbUpAltOutlinedIcon fontSize="large" />
+                                        <ThumbDownAltOutlinedIcon fontSize="large" />
+                                        <KeyboardArrowDownOutlinedIcon fontSize="large" />
+                                    </div>
+                                    <div className={styles.name}>{card.name ? card.name : card.title}</div>
+                                    <p className={styles.overview}> {card.overview} </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
             
         </div>
     )
--- pages/homepage.js
+++ pages/homepage.js
@@ -9,9 +9,9 @@ function homepage() {
         <div className="homepage-app">
             <Header />
             <Bilboard url={requests.fetchNetflixOriginals}/>
-            <Rows title="NETFLIX ORIGINALS" url={requests.fetchNetflixOriginals} /> 
-            <Rows title="Trending Now" url={requests.fetchTrending}/> 
-            <Rows title="Horrors" url={requests.fetchHorrorMovies}/> 
+            <Rows title="NETFLIX ORIGINALS" rowName="netflix_originals_row" url={requests.fetchNetflixOriginals} /> 
+            <Rows title="Trending Now" rowName="trending_row" url={requests.fetchTrending}/> 
+            <Rows title="Horrors" rowName="horrors_row" url={requests.fetchHorrorMovies}/> 
 
         </div>
     )
