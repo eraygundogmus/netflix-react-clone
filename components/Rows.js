35144a3a21c4b6c6e6e12e7b853bbe28dbd243ad
eraygundogmus <gundogmuseray@gmail.com>
2021-03-03 02:03:02 +0300

    minor scss changes

--- components/Rows.js
+++ components/Rows.js
@@ -15,16 +15,15 @@ const img_url = "https://image.tmdb.org/t/p/w500/"
 const strShorter = function truncate(short, n){
     return short?.length > n ? short.substr(0, n-1) + "..." : short;}
 
+
+
 function Rows( {title, url, rowName} ) {
     const [request, setRequest] = useState([]);
-     const [features, setFeatures ] = useState({            
-        textAlign: 'center',
-/*         visibility: 'hidden', */
-/*         zIndex: '-1', */
-/*         backgroundColor: 'grey',
-        display: 'flex',
-        width: '300px' */
-/*         position: 'absolute' */
+     const [features, setFeatures ] = useState({ 
+        transform: 'scale(1.3)',
+        zIndex: '9999',
+        position: 'absolute'
+
     })
 
     useEffect(() => {
@@ -33,39 +32,31 @@ function Rows( {title, url, rowName} ) {
             setRequest(req.data.results)
             return req;
         }
-        
         fetchRowItems();
 //        console.log(request)
     },[url])
 
-// To show features, i need to add an onHover effect for every item which will change item's css to display features.
-// but the problem is, for every row and for every item, there should be a unique class so other's will not change.
-
-// solution is, giving different className for every row like row__container__horror! 
-
 
     function hoverHandler(e) {
           setTimeout(() => {
-/*             document.getElementsByClassName(`${e.target.nextSibling.className}`)[0].style.visibility = 'visible'
-             document.getElementsByClassName(`${e.target.nextSibling.className}`)[0].style.top = '0' 
-            e.target.style.position = 'absolute'
-           e.target.style.maxHeight = '500px' */
-          }, 500)
+            const sth = null
+
           
 //        document.getElementsByClassName(`${e.target.nextSibling.className}`)[0].style.position = 'absolute'
          
-    }
+    }, 500)}
 
-    function hoverLeaveHandler(e) {
-         setTimeout(() => {          
-/*              document.getElementsByClassName(`${e.target.nextSibling.className}`)[0].style.visibility = 'hidden'
+/*     function hoverLeaveHandler(e) {
+         setTimeout(() => {
+
+              document.getElementsByClassName(`${e.target.nextSibling.className}`)[0].style.visibility = 'hidden'
               e.target.style.position = 'unset'
-             e.target.style.maxHeight = '300px'  */}, 500)
+             e.target.style.maxHeight = '300px' , 500)
 
 
-    }
-
+    } */
     
+// style={features}
 
     return (
         <div className={`${rowName}_container`}>
@@ -74,9 +65,9 @@ function Rows( {title, url, rowName} ) {
                         <div className={styles.row}> 
                             {request.map((card) => (
                             <div className={styles.parent}> 
-                                <div key={card.id} className={styles.card}>
-                                    <img onMouseEnter={hoverHandler} onMouseLeave={hoverLeaveHandler} className={styles.posters} src={`${img_url}${card.poster_path}`}/>
-                                        <div className={styles.features} style={features} >
+                                <div onMouseEnter={hoverHandler}  id="card" key={card.id} className={styles.card}>
+                                    <img className={styles.posters} src={`${img_url}${card.poster_path}`}/>
+                                        <div className={styles.features} >
                                                 <div className={styles.buttons}>
                                                     <PlayCircleFilledOutlinedIcon fontSize="large"/>
                                                     <AddCircleOutlineOutlinedIcon fontSize="large" />
@@ -89,8 +80,9 @@ function Rows( {title, url, rowName} ) {
                                         </div>
                                 </div>
                             </div>   
-                                ))}
+                            ))}
                         </div> 
+
                 </div>
         </div>
     )
