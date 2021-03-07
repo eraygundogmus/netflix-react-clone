fd12c42d7bc9622076df99ba16bbfc914b0ad1cb
eraygundogmus <gundogmuseray@gmail.com>
2021-03-07 17:28:44 +0300

    fix issues on dinamic search bar

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,7 @@ import { LaptopWindows } from '@material-ui/icons';
 
 
 
+
 function Header() {
     const [search, setSearch] = useState(true)
 
@@ -26,10 +27,23 @@ function Header() {
     }, []);
 
 
+    useEffect(() => {
+        if (search)
+        window.addEventListener('click', (e) => {
+            if (e.target == document.getElementById("search_icon")) {
+                setSearch(false)} else if (document.getElementById("search_active")) {
+                    if (e.target !== document.getElementById("search_active")) {
+                        setSearch(true)
+                    }
+                }
+        });
+    }, []);
+
+
 
 
     return (
-        <div id="header"className={styles.header}>
+        <div id="header" className={styles.header}>
             <div className={styles.left}> 
             <Link href="/homepage">
             <img className={styles.logo} src="https://raw.githubusercontent.com/eraygundogmus/netflix-clone/main/assets/logo.png"/>
@@ -37,9 +51,11 @@ function Header() {
             <Navigation />
             </div>
             <div className={styles.right}>
-                    {search == true ? <img id='search_icon' onClick={() => setSearch(false)}className={styles.search} src="https://raw.githubusercontent.com/eraygundogmus/netflix-clone/885b5f0cc3726de2f215cabf40e86d51618a5c59/assets/search-icon.svg"/> : 
+
+                    {search == true ? <img id='search_icon' className={styles.search} src="https://raw.githubusercontent.com/eraygundogmus/netflix-clone/885b5f0cc3726de2f215cabf40e86d51618a5c59/assets/search-icon.svg"/> : 
                     <div  className={styles.search_active}><img className={styles.search} src="https://raw.githubusercontent.com/eraygundogmus/netflix-clone/885b5f0cc3726de2f215cabf40e86d51618a5c59/assets/search-icon.svg"/>
                     <input id="search_active" type='text' placeholder='Titles,people,genres'></input></div> }
+
                     <img className={styles.avatar} src="https://raw.githubusercontent.com/eraygundogmus/netflix-clone/main/assets/avatar.png"/>
             </div>
         
